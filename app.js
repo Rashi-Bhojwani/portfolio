603efbfb6b10fcfd4a765337b4e657521e1285a4
rashidoescode <rashi.bhojwani16@gmail.com>
2026-01-31 00:27:47 +0530

    Removed Session

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 var createError = require('http-errors');
 var express = require('express');
-const session = require("express-session")
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-require("dotenv").config()
+require("dotenv").config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+// static files
 app.use(express.static(path.join(__dirname, "assets")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-
-// view engine setup
+// view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -22,37 +22,20 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(
-  session({
-    secret: "portfolio_secret",
-    resave: false,
-    saveUninitialized: true,
-  })
-);
-
-
+// routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//   console.log("Server running on port", PORT);
-// });
-
-// catch 404 and forward to error handler
+// 404 handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
